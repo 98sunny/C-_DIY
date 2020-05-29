546cf9d45905f9ca9d2fc482ebf71af00a2b1b88
98sunny <sunnys.sir@gmail.com>
2020-05-29 15:31:39 -0400

    Project Simple Calculator
    Project Simple Calculator

---
+++ Week02Calculator/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace Week02Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.lbl4 = new System.Windows.Forms.Label();
+            this.calculate = new System.Windows.Forms.Button();
+            this.txtOp1 = new System.Windows.Forms.TextBox();
+            this.txtOperand = new System.Windows.Forms.TextBox();
+            this.txtOp2 = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(138, 50);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(76, 17);
+            this.lbl1.TabIndex = 0;
+            this.lbl1.Text = "Operand 1";
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Location = new System.Drawing.Point(141, 99);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(65, 17);
+            this.lbl2.TabIndex = 1;
+            this.lbl2.Text = "Operator";
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.Location = new System.Drawing.Point(141, 148);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(76, 17);
+            this.lbl3.TabIndex = 2;
+            this.lbl3.Text = "Operand 2";
+            // 
+            // lbl4
+            // 
+            this.lbl4.AutoSize = true;
+            this.lbl4.Location = new System.Drawing.Point(141, 224);
+            this.lbl4.Name = "lbl4";
+            this.lbl4.Size = new System.Drawing.Size(48, 17);
+            this.lbl4.TabIndex = 7;
+            this.lbl4.Text = "Result";
+            // 
+            // calculate
+            // 
+            this.calculate.Location = new System.Drawing.Point(141, 303);
+            this.calculate.Name = "calculate";
+            this.calculate.Padding = new System.Windows.Forms.Padding(12);
+            this.calculate.Size = new System.Drawing.Size(185, 57);
+            this.calculate.TabIndex = 8;
+            this.calculate.Text = "Calculate";
+            this.calculate.UseVisualStyleBackColor = true;
+            this.calculate.Click += new System.EventHandler(this.calculate_Click);
+            // 
+            // txtOp1
+            // 
+            this.txtOp1.Location = new System.Drawing.Point(269, 44);
+            this.txtOp1.Name = "txtOp1";
+            this.txtOp1.Size = new System.Drawing.Size(100, 22);
+            this.txtOp1.TabIndex = 9;
+            // 
+            // txtOperand
+            // 
+            this.txtOperand.Location = new System.Drawing.Point(269, 99);
+            this.txtOperand.Name = "txtOperand";
+            this.txtOperand.Size = new System.Drawing.Size(100, 22);
+            this.txtOperand.TabIndex = 10;
+            // 
+            // txtOp2
+            // 
+            this.txtOp2.Location = new System.Drawing.Point(269, 163);
+            this.txtOp2.Name = "txtOp2";
+            this.txtOp2.Size = new System.Drawing.Size(100, 22);
+            this.txtOp2.TabIndex = 11;
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(269, 224);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(100, 22);
+            this.txtResult.TabIndex = 12;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(373, 303);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Padding = new System.Windows.Forms.Padding(12);
+            this.btnExit.Size = new System.Drawing.Size(185, 56);
+            this.btnExit.TabIndex = 13;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtOp2);
+            this.Controls.Add(this.txtOperand);
+            this.Controls.Add(this.txtOp1);
+            this.Controls.Add(this.calculate);
+            this.Controls.Add(this.lbl4);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl1);
+            this.Name = "Form1";
+            this.Text = "Simple Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.Label lbl4;
+        private System.Windows.Forms.Button calculate;
+        private System.Windows.Forms.TextBox txtOp1;
+        private System.Windows.Forms.TextBox txtOperand;
+        private System.Windows.Forms.TextBox txtOp2;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Week02Calculator/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Week02Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       /* private void num1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }*/
+
+        private void calculate_Click(object sender, EventArgs e)
+        {
+            int num1, num2;
+            double ans=0;
+            String op;
+            num1 = Convert.ToInt32(txtOp1.Text);
+            num2 = Convert.ToInt32(txtOp2.Text);
+            op = txtOperand.Text;
+            switch (op) {
+                case "+":
+                    ans=num1 + num2;
+                    break;
+                case "-":
+                    ans = num1 - num2;
+                    break;
+                case "*":
+                    ans = num1 * num2;
+                    break;
+                case "/":
+                    ans = num1 / num2;
+                    break;
+                case "%":
+                    ans = num1 % num2;
+                    break;
+                case "^":
+                    ans = 1;
+                    for (int i = 0; i < num2; i++)
+                        ans *= num1;
+                    break;
+            }
+            txtResult.Text = ans.ToString();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
